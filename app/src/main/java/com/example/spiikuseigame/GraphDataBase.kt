87fbcc629f0c6621a004f599c5e70989c224b9a6
koshiro1417kukuti <hady.kou@hotmail.com>
2021-07-01 14:07:44 +0900

    motionLayoutを使用 画像動的処理 ImageViewを試験的使用 themes.xmlに変更有 ->上端のタイトルタグを削除(適宜変更可能) SurfaceView使用検討中

--- app/src/main/java/com/example/spiikuseigame/GraphDataBase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spiikuseigame
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Graph::class],version = 1)
-abstract class GraphDataBase: RoomDatabase() {
-    abstract fun GraphDAO(): GraphDAO
-}
