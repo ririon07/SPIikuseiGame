465163190fbced1bf0bc4ddc24fcf8324082f9e9
shissan <yamada132566@icloud.com>
2021-09-08 13:12:18 +0900

    コミット

---
+++ app/src/main/java/com/example/spiikuseigame/Answer.kt
@@ -0,0 +1,11 @@
+import com.example.spiikuseigame.R
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Answer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
--- app/src/main/java/com/example/spiikuseigame/Question2.kt
+++ app/src/main/java/com/example/spiikuseigame/Question2.kt
@@ -1,15 +1,26 @@
 package com.example.spiikuseigame
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class Question2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question2)
 
+        
+
+        val textView = findViewById<TextView>(R.id.question)
+        val ans1 = findViewById<Button>(R.id.ans1)
+        ans1.setOnClickListener {
+            textView.setText("Hello")
+        }
 
         //ホーム画面に遷移
         val backButton = findViewById<Button>(R.id.backbutton)
